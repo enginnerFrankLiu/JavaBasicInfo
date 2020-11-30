cdd0ef547bb6b0d285def1468231ec6d51d5f147
I529891 <marshmellow.liu@sap.com>
2020-11-30 13:10:31 +0800

    add code info.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import javax.net.ssl.SSLContext;
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+
 public class Main {
 
     /**
@@ -63,11 +68,47 @@ public class Main {
         }
     }
 
+    /**
+     * the differnent way to init enum sets.
+     */
+    static void infoMS() {
+        EnumSet<SeasonEnum> seasonEnums1, seasonEnums2, seasonEnums3, seasonEnums4;
+        seasonEnums1 = EnumSet.of(SeasonEnum.SPRING, SeasonEnum.SUMMER);
+        seasonEnums2 = EnumSet.complementOf(seasonEnums1);
+        seasonEnums3 = EnumSet.allOf(SeasonEnum.class);
+        seasonEnums4 = EnumSet.range(SeasonEnum.SPRING, SeasonEnum.WINTER);
+        System.out.println(seasonEnums1);
+        System.out.println("-----------");
+        System.out.println(seasonEnums2);
+        System.out.println("-----------");
+        System.out.println(seasonEnums3);
+        System.out.println("-----------");
+        System.out.println(seasonEnums4);
+    }
+
+    /**
+     *
+     */
+    static void enumMapInfo() {
+        Map<SeasonEnum, String> enumMap = new EnumMap<SeasonEnum, String>(SeasonEnum.class);
+        enumMap.put(SeasonEnum.SPRING, "春天");
+        enumMap.put(SeasonEnum.WINTER, "冬天");
+        enumMap.put(SeasonEnum.FALL, "秋天");
+        enumMap.put(SeasonEnum.SUMMER, "夏天");
+        System.out.println(enumMap);
+    }
+
+    /**
+     * information for test function.
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("application start.");
         // Fuck();
         // notifyByOrderStatus(OrderStatusEnum.SEND);
+//        infoMS();
+//        enumMapInfo();
         System.out.println("application end.");
     }
 

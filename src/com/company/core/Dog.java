62ec743ef936de8092c01fe4e74543e16824fa5d
I529891 <marshmellow.liu@sap.com>
2021-05-11 09:49:22 +0800

    get good answer about string pool.

--- src/com/company/core/Dog.java
+++ src/com/company/core/Dog.java
@@ -1,5 +1,7 @@
 package com.company.core;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import javax.print.DocFlavor;
 
 public class Dog {
@@ -225,6 +227,21 @@ public class Dog {
     /**
      *otherwise a new string object will be added in String pool and the reference of this String is returned.
      *
+     * 驻留的字符串是放在全局共享的字符串常量池中的:
+     *
+     * 1）类加载对一个类只会进行一次。”xyz”在类加载时就已经创建并驻留了
+     * （如果该类被加载之前已经有”xyz”字符串被驻留过则不需要重复创建用于驻留的”xyz”实例）。驻留的字符串是放在全局共享的字符串常量池中的。
+     *
+     * （2）在这段代码后续被运行的时候，”xyz”字面量对应的String实例已经固定了，
+     * 不会再被重复创建。所以这段代码将常量池中的对象复制一份放到heap中，并且把heap中的这个对象的引用交给s1持有
+     *
+     * "常量池中的对象复制一份放到heap中"
+     *
+     * 常量池中的对象，只的是，“A” 还是 “A” 在 string pool 中的引用.
+     * 这里，我暂且理解成，“A” 在string pool 中的引用吧
+     *
+     * 运行期间，将常量放入到常量池，怎么去理解这句话
+     *
      */
     public void fuck1(){
 
@@ -232,4 +249,72 @@ public class Dog {
         String b=a.intern();
         System.out.println(a==b);
     }
+
+    public void finalFuck(){
+        String a=new String("INFO");
+        String b=a.intern();
+        String c="INFO";
+        System.out.println(a==b); //false
+        System.out.println(a==c); //true
+    }
+
+    /**
+     * 这里还有另外的一种说法:
+     * 在HotSpot VM里实现的string pool功能的是一个StringTable类，
+     * 它是一个哈希表，里面存的是驻留字符串(也就是我们常说的用双引号括起来的)的引用（而不是驻留字符串实例本身），
+     * 也就是说在堆中的某些字符串实例被这个StringTable引用之后就等同被赋予了”驻留字符串”的身份。这个StringTable在每个HotSpot VM的实例只有一份，被所有的类共享。
+     * 总的来说，这个链接，将的还不错：https://www.jb51.net/article/171127.htm
+     *
+     * 全局字符串常量池中存放的内容是在类加载完成后存到String Pool中的，在每个VM中只有一份，存放的是字符串常量的引用值(在堆中生成字符串对象实例)。
+     *
+     * https://zhuanlan.zhihu.com/p/64839455
+     *
+     * 运行时常量池是在类加载完成之后，将每个class常量池中的符号引用值转存到运行时常量池中，也就是说，每个class都有一个运行时常量池，类在解析之后，将符号引用替换成直接引用，与全局常量池中的引用值保持一致。
+     *
+     */
+    public void summary(){
+
+    }
+
+
+    /**
+     *
+     *https://zhuanlan.zhihu.com/p/64839455
+     *
+     * 示例中首先经过编译之后，在该类的class常量池中存放一些符号引用，然后类加载之后，
+     * 将class常量池中存放的符号引用转存到运行时常量池中，然后经过验证，准备阶段之后，
+     * 在堆中生成驻留字符串的实例对象(也就是上例中str1所指向的“abc”实例对象)，
+     * 然后将这个对象的引用存到全局String Pool中，也就是String Pool中，
+     * 最后在解析阶段，要把运行时常量池中的符号引用替换成直接引用，
+     * 那么就直接查询String Pool，
+     * 保证String Pool里的引用值与运行时常量池中的引用值一致，大概整个过程就是这样了。
+     *
+     * 回到示例的那个程序，现在就很容易解释整个程序的内存分配过程了，首先，
+     * 在堆中会有一个“abc”实例，全局String Pool中存放着“abc”的一个引用值，
+     * 然后在运行第二句的时候会生成两个实例，一个是“def”的实例对象，
+     * 并且String Pool中存储一个“def”的引用值，还有一个是new出来的一个“def”的实例对象，
+     * 与上面那个是不同的实例，当在解析str3的时候查找String Pool，里面有“abc”的全局驻留字符串引用，
+     * 所以str3的引用地址与之前的那个已存在的相同，str4是在运行的时候调用intern()函数，
+     * 返回String Pool中“def”的引用值，如果没有就将str2的引用值添加进去，
+     * 在这里，String Pool中已经有了“def”的引用值了，
+     * 所以返回上面在new str2的时候添加到String Pool中的 “def”引用值，
+     * 最后str5在解析的时候就也是指向存在于String Pool中的“def”的引用值，那么这样一分析之后，结果就容易理解了。
+     *
+     *
+     */
+    public void infoqd(){
+
+
+        String str1 = "abc";
+        String str2 = new String("def");
+        String str3 = "abc";
+        String str4 = str2.intern();
+        String str5 = "def";
+        System.out.println(str1 == str3);// true
+        System.out.println(str2 == str4);// false
+        System.out.println(str4 == str5);// true
+    }
+
+
+
 }

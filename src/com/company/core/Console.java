3d64e6975c04d24e9716553f38b08c151599e7eb
enginnerFrankLiu <enginnerFrankLiu@icloud.com>
2021-06-23 22:32:58 +0800

    log

--- src/com/company/core/Console.java
+++ src/com/company/core/Console.java
@@ -2,11 +2,8 @@ package com.company.core;
 
 import com.company.basic.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Console {
     public static void log() {
